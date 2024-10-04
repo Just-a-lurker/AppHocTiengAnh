9715f2c203b50c880a3d475713ebcd81fd1d5892
Just-a-lurker <hoangmegamanxyz@gmail.com>
2024-10-04 14:53:50 +0700

    update

--- FirebaseProjectStudyEnglish/app/src/main/java/hmd/example/firebaseprojectstudyenglish/MainActivity.java
+++ FirebaseProjectStudyEnglish/app/src/main/java/hmd/example/firebaseprojectstudyenglish/MainActivity.java
@@ -59,7 +59,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Mọi thắc mắc, hỗ trợ xin vui lòng gửi mail về (huynhminhduc@gmail.com)", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Mọi thắc mắc, hỗ trợ xin vui lòng gửi mail về (developers@gmail.com)", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
